cc9722d8f56020b716bb12c640c8fd4a491286bf
sophiecwebster <sophiecwebster@gmail.com>
2020-07-07 12:50:09 -0400

    update file tree

--- sw-ipyrad.sh
+++ sw-ipyrad.sh
@@ -2,6 +2,7 @@
 
 # /n/holyscratch01/hopkins_lab/webster/
 # ├── ipyrad/                             (all shell/python scripts and instructions)
+# │    └── trimmomatic/                    (trimmomatic tools)
 # └── radseq/                             (original zip file and (partially) unzipped fastq gzip files)
 #     ├── demultiplex/                    (entirety of step 2b's results, all demultiplexed files in stacks and rem)
 #     └── splits/                         (the 'aa, ab, etc' type fastq files, split for parallel processing)
@@ -215,3 +216,25 @@ mv: cannot stat ‘demultiplex/stacks_2CF/*.2.fq’: No such file or directory
 39422359 retained reads (77.5%)
 
 # -------- #
+
+# step 2c: trimming
+
+#!/bin/bash
+
+# do this on an interactive node
+srun -p test -n 1 -N 1 --pty --mem 4000 -t 0-04:00 /bin/bash
+
+# load Java module
+module load jdk/10.0.1-fasrc01
+
+# set working directory
+cd /n/holyscratch01/hopkins_lab/[you]/ipyrad/trimmomatic
+
+# download trimmomatic
+wget http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/Trimmomatic-0.35.zip
+
+# unzip
+unzip Trimmomatic-0.35.zip
+
+# move illumina parameters file to working directory
+cp /n/holyscratch01/hopkins_lab/[you]/ipyrad/trimmomatic/Trimmomatic-0.35/adapters/TruSeq3-PE.fa /n/holyscratch01/hopkins_lab/[you]/ipyrad/trimmomatic/ ### this just puts this file into your working directory
